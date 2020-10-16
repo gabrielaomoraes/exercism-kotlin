0b9a22f3b55c132fb3f0ab7efe9f57c4b9cd9169
gabrielaomoraes <gabriela.omoraes@gmail.com>
2020-10-16 14:11:28 -0300

    Dnd character resolution

---
+++ src/main/kotlin/dnd/character/DndCharacter.kt
@@ -0,0 +1,33 @@
+package dnd.character
+
+import kotlin.math.floor
+
+class DndCharacter {
+
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = 10 + modifier(constitution)
+
+    companion object {
+
+        fun ability(): Int {
+
+            val s = generateSequence { Dice(6)
+                .roll(); Dice(6).roll(); Dice(6)
+                .roll(); Dice(6).roll() }
+            return s.sorted().drop(1).sum()
+        }
+
+        fun modifier(abilityScore: Int) = floor((abilityScore - 10).toDouble() / 2).toInt()
+
+    }
+
+}
+
+class Dice(private val sides: Int) {
+    fun roll() = (0..sides).random()
+}
